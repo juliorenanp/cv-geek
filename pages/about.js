c08ae0b70940c8494fc5212aeaf59305bc35d5cd
Julio Pinheiro <juliorenan@live.com>
2022-07-18 19:54:06 -0300

    fix: fix the about text

--- pages/about.js
+++ pages/about.js
@@ -5,16 +5,15 @@ export default function About() {
     <div className="nes-container with-title ">
       <p className="title">About me</p>
       <p>
-        Hi, I'm Julio Pinheiro and I live in São Paulo, Brazil. I've been a
-        software developer for the past nine years! I've been working in several
+        Hi, my name is Julio Pinheiro and I live in São Paulo, Brazil. I've been
+        a software developer for the past nine years! and working in several
         business segments and with very cool and interesting technologies. I
         learned a lot during my career, and what I value the most is
         understanding the real needs of the clients/business. I also had the
         great opportunity to lead teams during some projects, understanding the
-        profiles of each one, and feedback techniques. I really enjoy teaching,
-        especially to people new to the area, I had several nice opportunities
-        in this regard. I also like to learn from the team and keep the team
-        engaged.
+        profiles of each one. I really enjoy teaching, especially to people new
+        to the area, I had several nice opportunities in this regard. I also
+        like to learn from the team and keep the team engaged.
       </p>
     </div>
   );
--- pages/header.js
+++ pages/header.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 export default function Header() {
-    return (
-        <div className="nes-container with-title is-centered">
-        <p className="title">Welcome! Hello! </p>
-          This is my creative resume! Hope you like it!{" "}
-        <i className="nes-icon  like"></i>
-      </div>
-    );
+  return (
+    <div className="nes-container with-title is-centered">
+      <p className="title">Welcome 🤘 </p>
+      This is my creative resume! Hope you like it!{" "}
+      <i className="nes-icon  like"></i>
+    </div>
+  );
 }
-
