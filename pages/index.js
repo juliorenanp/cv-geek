51c444a58a8ca5cc9b0068bb721fa502bd028fe1
Julio Pinheiro <juliorenan@live.com>
2022-07-18 19:38:36 -0300

    fix: fixing header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import "nes.css/css/nes.min.css";
+import styles from "../styles/Home.module.css";
+import Header from "./header";
 
 export default function Home() {
   return (
@@ -11,11 +12,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-      <h2>TESTERRR</h2>
-       
-      </main>
+    <header>
+    <Header />
+    </header>
 
+      <main>
+      
+      </main>
     </div>
-  )
+  );
 }
