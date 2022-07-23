786b0b08d499f01060414a4196fabc1a32a2bbc7
Julio Pinheiro <juliorenan@live.com>
2022-07-22 23:40:25 -0300

    feat: correct contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,35 +1,39 @@
 import React from "react";
 
 export default function Contact() {
-    return (
-      <div className="nes-container with-title" id="contact">
-        <p className="title">Contact</p>
+  return (
+    <div className="nes-container with-title" id="contact">
+      <p className="title">Contact</p>
 
-        <section className="nes-container">
-          <section className="message-list">
-            <section className="message -left">
-              <i className="nes-mario"></i>
-              <div className="nes-balloon from-left">
-                <p>Nice CV Julio, can you pass me your contact?</p>
-              </div>
-            </section>
-            <section className="message -right">
-              <div className="nes-balloon from-right">
-                <p>
-                  For sure my friend:{" "}
-                  <a href="https://github.com/juliorenanp">
-                    <i className="nes-icon github"></i>
-                  </a>{" "}
-                  <a href="https://www.linkedin.com/in/juliorspinheiro/">
-                    <i className="nes-icon linkedin"></i>
-                  </a>
-                </p>
-              </div>
-              <i className="nes-squirtle"></i>
-            </section>
+      <section className="nes-container">
+        <section className="message-list">
+          <section className="message -left">
+            <div className="nes-balloon from-left">
+              <p>Nice CV Julio, can you pass me your contact?</p>
+            </div>
           </section>
+          <i className="nes-mario"></i>
+          <section className="message -left">
+            <div className="nes-balloon from-left">
+              <p>
+                For sure my friend:{" "}
+                <a href="https://github.com/juliorenanp">
+                  <i className="nes-icon github"></i>
+                </a>{" "}
+                <a href="https://www.linkedin.com/in/juliorspinheiro/">
+                  <i className="nes-icon linkedin"></i>
+                </a>
+              </p>
+            </div>
+          </section>
+          <div>
+          <i className="nes-ash"></i> {"  "}
+          <i className="nes-charmander"></i>{"  "}
+          <i className="nes-squirtle"></i>
+          <i className="nes-bulbasaur"></i>{"  "}
+          </div>
         </section>
-      </div>
-    );
+      </section>
+    </div>
+  );
 }
-
