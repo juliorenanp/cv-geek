948a17653eeaff414fa60f0e57ea12e855ab313c
Julio Pinheiro <juliorenan@live.com>
2022-07-22 22:57:56 -0300

    style: about

--- pages/about.js
+++ pages/about.js
@@ -5,11 +5,11 @@ export default function About() {
     <div className="nes-container with-title" id="about">
       <p className="title">About me</p>
       <p>
-        Hi, my name is Julio Pinheiro and I live in São Paulo, Brazil. {"I've"}  been
+        Hi, my name is Julio Pinheiro and I live in São Paulo, Brazil 🇧🇷. {"I've"}  been
         a software developer for the past nine years! and working in several
         business segments and with very cool and interesting technologies. I
         learned a lot during my career, and what I value the most is
-        deliver the real needs of the clients/business. I also had the
+        delivering the real needs of the clients/business. I also had the
         great opportunity to lead teams during some projects, understanding the
         profiles of each one. I really enjoy teaching, especially to people new
         to the area, I had several nice opportunities in this regard. I also
