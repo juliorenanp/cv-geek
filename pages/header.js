f0c51540ef24f38f7c3908770e37a005c1d03b37
Julio Pinheiro <juliorenan@live.com>
2022-07-22 22:56:13 -0300

    feat: header

--- pages/header.js
+++ pages/header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="nes-container with-title is-centered">
-      <p className="title">Welcome 🤘 </p>
+      <p className="title">Welcome!</p>
       This is my creative resume! Hope you like it!{" "}
       <i className="nes-icon  like"></i>
       <p>
