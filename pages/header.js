93ee061d128a68edb00ebb6a6640a472a5dee125
Julio Pinheiro <juliorenan@live.com>
2022-08-17 20:46:40 -0300

    fix: header fix:

--- pages/header.js
+++ pages/header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="nes-container with-title is-centered">
       <p className="title">Welcome!</p>
-      This is my creative resume! Hope you like it!{" "}
+      This is my geek resume! Hope you like it!{" "}
       <i className="nes-icon  like"></i>
       <p>
         <a href="#about">About me </a> - {" "}
