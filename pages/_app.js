d1a7e98621d644d238f1485564b3b37b3552d9f3
Julio Pinheiro <juliorenan@live.com>
2024-01-18 23:12:49 +0000

    added `react-ga`

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
+import ReactGA from 'react-ga';
 
 function MyApp({ Component, pageProps }) {
+  ReactGA.initialize('G-2VFX7QGY88');
   return <Component {...pageProps} />
 }
 
