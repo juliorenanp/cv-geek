b5d3ee424d876667629aa49c2636f70b180ca054
Julio Pinheiro <julio.pinheiro@xfive.co>
2024-07-08 20:46:13 -0700

    feat: added new colors for links.

--- pages/header.js
+++ pages/header.js
@@ -3,15 +3,33 @@ import React from "react";
 export default function Header() {
   return (
     <div className="nes-container with-title is-centered">
-      <p className="title">Welcome!</p>
+      <p className="title nes-black">Welcome!</p>
       This is my geek resume! Hope you like it!{" "}
       <i className="nes-icon  like"></i>
       <p>
-        <a href="#about">About me</a> - <a href="#skill">Skills</a> -{" "}
-        <a href="#experience">Experience</a> -{" "}
-        <a href="#education">Education</a> -{" "}
-        <a href="#projects">Personal Projects</a> -{" "}
-        <a href="#contact">Contact</a>
+        <a href="#about" className="secondary">
+          About me
+        </a>{" "}
+        -{" "}
+        <a href="#skill" className="secondary">
+          Skills
+        </a>{" "}
+        -{" "}
+        <a href="#experience" className="secondary">
+          Experience
+        </a>{" "}
+        -{" "}
+        <a href="#education" className="secondary">
+          Education
+        </a>{" "}
+        -{" "}
+        <a href="#projects" className="secondary">
+          Personal Projects
+        </a>{" "}
+        -{" "}
+        <a href="#contact" className="secondary">
+          Contact
+        </a>
       </p>
     </div>
   );
